1a0358ee118ffb8ecfd99e7bb45f04dacf83bfd4
zym <minititanzym@gmail.com>
2019-07-27 11:57:53 +0800

    Updates

---
+++ precache-manifest.408deb6f03102cf72543b4d625a522b9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be7e0eedc099f9928831c3e55eecda8",
+    "url": "/jianshu/index.html"
+  },
+  {
+    "revision": "83753f96e305a7704b27",
+    "url": "/jianshu/static/js/2.a1f8a1ee.chunk.js"
+  },
+  {
+    "revision": "8b335f9ccf20bcdf904c",
+    "url": "/jianshu/static/js/main.7fbc9ecf.chunk.js"
+  },
+  {
+    "revision": "74126ff601443a39a334",
+    "url": "/jianshu/static/js/runtime~main.362de7da.js"
+  }
+]);
